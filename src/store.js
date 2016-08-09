d093fb1b9b727092e2a41843f13b02e282ea5f68
Kincaid O'Neil <kincaidoneil@gmail.com>
2016-08-09 08:17:14 -0500

    Add store for shared settings across modules

---
+++ src/store.js
@@ -0,0 +1,18 @@
+export default {
+  classes: {
+    1: 'Free Block',
+    2: 'Spanish',
+    3: 'AP Stats',
+    4: 'AP Physics C',
+    5: 'AP Gov',
+    6: 'Econ CIS',
+    7: 'Debate'
+  },
+  lunches: {
+    Monday: 1,
+    Tuesday: 1,
+    Wednesday: 2,
+    Thursday: 2,
+    Friday: 1
+  }
+}
