3b8392c545fd9a1e6b8c39637b7ca16dc1b3f090
Kincaid O'Neil <kincaidoneil@gmail.com>
2016-09-07 22:19:43 -0500

    Create Day component, add all the things!
    App.vue
    - Countdown logic is now inside Day.vue
    - Restoring prefs has been moved to store.js
    - Move toggling lunch to Day component
    - Remove filters, use computed properties in Day instead
    - Clean up imports
    - Remove footer text (temporary)
    - Add spinner while request is loading
    - Clean up all styling
    - Add some slick list transitions when lunches are toggled!
    
    Day.vue
    - Move everything from App component
    - Styles are much more organized
    - Dim days that aren’t today
    - Increase the width of the current day
    - Move countdown to right side of current block, outside of its bounds
    - Add ‘Passing’ text during passing time
    - Advisories are now proportional, there’s just no text—it’s just a
    solid bar
    - Reset the countdown whenever _schedule changes, preventing the
    previously queued call to run
    - Improve countdown performance by caching the index of each block,
    queuing each refresh directly after the next second, and only
    calculating the difference between the end time and now
    - Also, don’t refresh the countdown if the school day is over
    - Tab title text is now more description, i.e. “3:28 until Econ CIS”

--- src/App.vue
+++ src/App.vue
@@ -7,173 +7,79 @@
     height: 100%
     margin: 0
     padding: 0
-    background: #546E7A
-    display: flex
-    align-items: center
-    justify-content: center
-    overflow-x: hidden
-    overflow-y: auto
+    background: #262626
     font-styling()
+    
+  #container
+    width: 100%
+    height: 100%
+    display: flex
+    flex-direction: column
+    justify-content: center
+    flex-align: center
     
   .header
     width: 100%
-    margin-bottom: 40px
+    margin-bottom: 30px
     font-weight: 100
     font-size: 64px
+    line-height: 64px
     text-align: center
     
-  #schedule
-    display: flex
+  .fade-enter-active, .fade-leave-active
+    transition: all 0.3s
     
-    .dayContainer
-      width: 180px
-      margin-right: 10px
-      flex-grow: 1
-      display: flex
-      flex-direction: column
-      justify-content: space-between
+  .fade-enter, .fade-leave-active
+    opacity: 0
+    
+  #loading
+    width: 100%
+    height: 100%
+    display: flex
+    align-items: center
+    justify-content: center
+    
+    #spinner
+      width: 80px
+      height: 80px
+      box-sizing: border-box
+      border-radius: 50%
+      border-bottom: 6px solid accentColor
+      -webkit-animation: rotate 1s
+      animation: rotate 1s
       
-      .dayHeader
-        width: 100%
-        padding: 6px 6px 8px 6px
-        box-sizing: border-box
-        align-self: flex-start
-        text-align: center
-        font-size: 18px
-        
-      .day
-        width: 100%
-        align-self: flex-end
-        
-        .blockContainer
-          width: 100%
-          float: left
+      @keyframes rotate
+        from
+          transform: rotate(0deg)
+        to
+          transform: rotate(360deg)
           
-          .beforeBlock
-            width: 100%
-            margin: calc(5px * 0.5 * 1.2) 0
-            float: left
-            
-          .block
-            width: 180px
-            padding: 6px
-            border-radius: 4px
-            float: left
-            box-sizing: border-box
-            background: darkgray
-            
-            .blockHeader
-              width: 100%
-              height: 20px
-              float: left
-              display: flex
-              flex-direction: row
-              line-height: 20px
-              
-              .blockNum
-                margin: 0
-                flex-shrink: 1
-                margin-right: 5px
-                font-weight: bold
-                
-              .blockName
-                flex-grow: 1
-                font-weight: 300
-                
-                .lunch
-                  text-decoration: underline
-                  cursor: pointer
-                  
-              .blockInput
-                width: 100%
-                height: 20px
-                margin: 0
-                padding: 0
-                flex-grow: 1
-                border: none
-                outline: none
-                background: transparent
-                font-styling()
-                font-weight: 300
-                line-height: 20px
-                
-              :focus
-                border-bottom: 2px solid white
-                
-            .classTimes
-              width: 100%
-              margin-top: 2px
-              float: left
-              font-size: 12px
-                
+  #week
+    display: flex
+    align-items: stretch
+    justify-content: center
+    flex-direction: row
+    
+    .day
+      margin-right: 6px
+      
     :last-of-type
       margin-right: 0
-      
-    .countdown
-      width: 100%
-      margin: 2px 0
-      float: left
-      line-height: 24px
-      font-size: 24px
-      font-weight: 300
-      
-      
-  footer
-    width: 100%
-    height: 60px
-    font-size: 12px
-    font-weight: 200 !important
-    display: flex
-    justify-content: flex-end
-    flex-direction: column
-    
-    p
-      margin: 0
-      text-align: center
-      
-  a
-    color: white
-      
+        
 </style>
 <template>
   <div id="container">
-    <div class="header">BearTime</div>
-    <div id="schedule">
-      <div class="dayContainer" v-for="(day, i) in shared.week">
-        <header class="dayHeader">{{ displayDate | dateText(i) }}</header>
-        <div class="day">
-          <div class="blockContainer" v-for="(block, j) in _schedule[day]">
-            <div class="beforeBlock" v-if="j !== 0">
-              <div class="countdown" v-if="block._id === _countdown._id && _countdown.before">
-                {{ _countdown.text }}
-              </div>
-            </div>
-            <div class="block" :style="{ minHeight: String(block.duration * shared.sizing) + 'px',
-              background: shared.colors[block.number - 1] }">
-              <header class="blockHeader">
-                <span class="blockNum" v-if="block.number">{{ block.number }}</span>
-                <span class="blockName" v-if="block.name && !block.lunch">{{ block.name }}</span>
-                <span class="blockName" v-if="block.name && block.lunch">
-                  <span class="lunch" @click="toggleLunch(day, block.lunch)"
-                    title="Toggle Lunch">{{ block.lunch | lunchText }}</span> {{ block.name }}
-                </span>
-                <input class="blockInput" v-if="!block.name" v-model="shared.classes[block.number]"
-                  :autofocus="i === 0 && block.number === 1 && !shared.classes[block.number]" />
-              </header>
-              <div class="countdown" v-if="block._id === _countdown._id && !_countdown.before">
-                {{ _countdown.text }}
-              </div>
-              <div class="classTimes" v-else v-if="block.duration >= 45">
-                {{ block.start }}-{{ block.end }}
-              </div>
-            </div>
-          </div>
-        </div>
+    <transition name="fade" mode="out-in">
+      <div v-if="loading" id="loading">
+        <div id="spinner"></div>
       </div>
-    </div>
-    <footer>
-      <p>Created by <a href="https://kincaidoneil.com">Kincaid O'Neil</a> and <a href="#">Saji Champlin</a></p>
-    </footer>
+      <div v-else id="schedule">
+        <h1 class="header">BearTime</h1>
+        <transition-group name="fade" tag="div" id="week" appear>
+          <Day v-for="date in week" :date="date" :key="date" class="day"></Day>
+        </transition-group>
+      </div>
+    </transition>
   </div>
 </template>
 <script>
@@ -181,50 +87,27 @@
   import Vue from 'vue'
   import VueResource from 'vue-resource'
   Vue.use(VueResource)
-  import _merge from 'lodash/merge'
-  import moment from 'moment'
-  import 'velocity-animate'
   
-  // Map lunch number to the text version
-  Vue.filter('lunchText', lunch => {
-    return (lunch === 1) ? 'First' : 'Second'
-  })
-  // Return header text with day and date, corresponding to displayDate
-  Vue.filter('dateText', (displayDate, i) => {
-    return moment(displayDate).day(i + 1).format('dddd, M/D')
-  })
+  import enquire from 'enquire.js'
+  import _uniqueId from 'lodash/uniqueId'
+  import moment from 'moment'
+  import 'moment-duration-format'
+  
   import store from './store'
+  import Day from './Day.vue'
   
   export default {
-    data() {
-      return {
-        // Add all methods and data from beartime-core! Yay :D
-        shared: beartime,
-        // Check if client is crawler so it doesn't see a countdown in the title!
-        isCrawler: /bot|googlebot|crawler|spider|robot|crawling/i.test(navigator.userAgent),
-        // Set current time (as a UNIX timestamp to trigger countdown updates)
-        // Moment date objects won't work!
-        now: moment().valueOf(),
-        // Set default date to determine week to display
-        displayDate: moment().valueOf()
-      }
-    },
+    components: { Day },
     data: () => store,
     watch: {
-      // Update title of page with current time (a full old BearTime replacement!)
-      '_countdown.text'() {
-        if (this.isCrawler) return
-        let text = this._countdown.text
-        document.title = (text) ? `${text} \u2022 BearTime` : 'BearTime'
-      },
       // Update prefs in localStorage when bindings change
-      'shared.lunches': {
+      'lunches': {
         handler() {
           this.updatePrefs('lunches')
         },
         deep: true
       },
-      'shared.classes': {
+      'classes': {
         handler() {
           this.updatePrefs('classes')
         },
@@ -232,26 +115,10 @@
       }
     },
     mounted() {
-      // Load saved class names and first/second lunch preferences for each day
-      this.restorePrefs('lunches')
-      this.restorePrefs('classes')
       // Load initial schedule
       this.fetchSchedule()
     },
     methods: {
-      toggleLunch(day, oldLunch) {
-        let newLunch = (oldLunch === 1) ? 2 : 1
-        this.shared.lunches[day] = newLunch
-      },
-      updateTime() {
-        // Set timer to update at the very next second
-        setTimeout(() => {
-          this.now = moment().valueOf()
-        }, 1000 - moment().millisecond())
-      },
-      restorePrefs(key) {
-        _merge(this.shared[key], JSON.parse(localStorage.getItem(key)) || {})
-      },
       updatePrefs(key) {
         localStorage.setItem(key, JSON.stringify(this[key]))
       },
@@ -289,18 +156,7 @@
           let day = date.day(i).format('YYYY-MM-DD')
           week.push(day)
         }
-        return schedule
-      },
-      _countdown() {
-        // Queue subsequent countdown update
-        this.updateTime()
-        // If today is the displayDate and a schedule exists for today,
-        // update and show countdown (i.e., don't update if it's Saturday)
-        let displayDate = moment(this.displayDate)
-        let now = moment(this.now)
-        let schedule = this._schedule[now.format('dddd')]
-        return (displayDate.isSame(now, 'day') && schedule) ?
-          this.shared.getCountdown(this.now, schedule) : { show: false }
+        return week
       }
     }
   }
