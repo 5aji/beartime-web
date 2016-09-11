57d21540e74afbeb1d52a20d7631fe3107346437
Kincaid O'Neil <kincaidoneil@gmail.com>
2016-09-11 11:43:53 -0500

    Simplify restoration of preferences

--- src/store.js
+++ src/store.js
@@ -2,16 +2,15 @@ import _merge from 'lodash/merge'
 import moment from 'moment'
 
 let today = moment().format('YYYY-MM-DD')
+let restorePrefs = (key, defaults) => _merge(defaults, JSON.parse(localStorage.getItem(key)) || {})
 
 export default {
   dimming: false,
   loading: true,
   schedule: {},
-  // Restore saved preferences to over default lunches and classes
-  lunches: _merge({ 'Monday': 1, 'Tuesday': 1, 'Wednesday': 1, 'Thursday': 1, 'Friday': 1 },
-    JSON.parse(localStorage.getItem('lunches')) || {}),
-  classes: _merge({ 1: '', 2: '', 3: '', 4: '', 5: '', 6: '', 7: '' },
-    JSON.parse(localStorage.getItem('classes')) || {}),
+  // Restore saved preferences to overwrite default lunches and classes
+  lunches: restorePrefs('lunches', { 'Monday': 1, 'Tuesday': 1, 'Wednesday': 1, 'Thursday': 1, 'Friday': 1 }),
+  classes: restorePrefs('classes', { 1: '', 2: '', 3: '', 4: '', 5: '', 6: '', 7: '' }),
   // Design constants
   sizing: 1.2,
   colors: ['#3F51B5', '#1976D2', '#039BE5', '#00BCD4', '#009688', '#43A047', '#7CB342'],
