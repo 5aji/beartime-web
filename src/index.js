7cbdf3f79040b76377a5b83e6217f510277f2541
Kincaid O'Neil <kincaidoneil@gmail.com>
2017-05-25 12:37:03 -0500

    Change backend to Firebase
    - Just use REST since a constantly open socket isn't helpful when the data is pretty static
    - Run media query on resize of window and on load
    - Trigger action to update week, schedule and begin countdown

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
 import Vue from 'vue'
-import App from './App.vue'
+import axios from 'axios'
+import App from '@/components/App.vue'
+import store from '@/store/'
+import {mapActions, mapMutations} from 'vuex'
 
 new Vue({
+  store,
   el: '#app',
-  render: h => h(App)
+  render: h => h(App),
+  async created() {
+    this.goToday()
+    // Show/hide week on load
+    this.changeView()
+    // When the window is resized, update media query automatically
+    window.addEventListener('resize', this.changeView)
+    // Collect special schedules; can't really do anything about an error anyways...
+    const {data} = await axios.get('https://beartime-9facf.firebaseio.com/specials.json')
+    this.SET_SPECIALS(data)
+    this.SET_LOADING(false)
+    this.updateWeek()
+  },
+  methods: {
+    ...mapActions(['changeView', 'updateWeek', 'goToday']),
+    ...mapMutations(['SET_LOADING', 'SET_SPECIALS'])
+  }
 })
